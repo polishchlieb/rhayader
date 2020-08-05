ec7882fdb676b05cfb7290a94d1515b54854bbb5
polishchlieb <biczu.yt@gmail.com>
2020-08-06 01:16:26 +0200

    grouped into class

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 int main() {
-    const auto tokens = tokenizer::tokenize("2 + 2");
+    tokenizer::Tokenizer t;
+    const auto tokens = t.tokenize("1 + 2 - 3 * 4 / 5");
     for (const auto& token : tokens) {
         std::cout << "(" << tokenizer::dumpTokenType(token.type) << ") " << token.value << std::endl;
     }
+
     return 0;
 }
--- tokenizer/tokenizer.hpp
+++ tokenizer/tokenizer.hpp
@@ -8,93 +8,128 @@
 #include "PendingToken.hpp"
 
 namespace tokenizer {
-    PendingTokenType getType(const char c) {
-        if (isdigit(c)) return PendingTokenType::number;
-        if (isalpha(c)) return PendingTokenType::name;
-        if (c == ' ') return PendingTokenType::none;
-        if (c == '\'' || c == '"') return PendingTokenType::string;
-        if (c == '+') return PendingTokenType::add_op;
-        if (c == '-') return PendingTokenType::sub_op;
-        if (c == '*') return PendingTokenType::mul_op;
-        if (c == '/') return PendingTokenType::div_op;
-        return PendingTokenType::unknown;
-    }
+    class Tokenizer {
+    public:
+        std::vector<Token> tokenize(const std::string& line) {
+            for (const char c : line) {
+                parseChar(c);
+            }
 
-    std::vector<Token> tokenize(const std::string& line) {
+            if (!previous.isEmpty())
+                tokens.push_back(previous.toToken());
+
+            return tokens;
+        }
+    private:
         std::vector<Token> tokens;
-        PendingToken current{PendingTokenType::none, ""};
+        PendingToken previous{PendingTokenType::none, ""};
 
-        for (const char c : line) {
-        iteration:
-            if (current.type == PendingTokenType::none) {
-                const auto type = getType(c);
-                switch (getType(c)) {
-                    case PendingTokenType::number:
-                    case PendingTokenType::name:
-                    case PendingTokenType::add_op:
-                    case PendingTokenType::sub_op:
-                    case PendingTokenType::div_op:
-                    case PendingTokenType::mul_op:
-                        current.type = type;
-                        current.value += c;
-                        break;
-                    case PendingTokenType::string:
-                    case PendingTokenType::unknown:
-                        current.type = type;
-                        break;
-                    case PendingTokenType::none:
-                        break;
-                }
-                continue;
-            }
-
-            if (current.type == PendingTokenType::string) {
-                if (c == '\'' || c == '"') {
-                    tokens.push_back(current.toToken());
-                    current.clear();
-                    continue;
-                }
-
-                current.value += c;
-                continue;
-            }
-
-            if (current.type == PendingTokenType::number) {
-                const auto type = getType(c);
-                if (type == PendingTokenType::number) {
-                    current.value += c;
-                    continue;
-                }
-
-                tokens.push_back(current.toToken());
-                current.clear();
-                goto iteration;
-            }
-
-            if (current.type == PendingTokenType::name) {
-                const auto type = getType(c);
-                if (type == PendingTokenType::name || type == PendingTokenType::number) {
-                    current.value += c;
-                    continue;
-                }
-
-                tokens.push_back(current.toToken());
-                current.clear();
-                goto iteration;
-            }
-
-            if (current.type == PendingTokenType::add_op) {
-                tokens.push_back(current.toToken());
-                current.clear();
-                goto iteration;
+        void parseChar(const char c) {
+            switch (previous.type) {
+                case PendingTokenType::none: parseNone(c); break;
+                case PendingTokenType::string: parseString(c); break;
+                case PendingTokenType::number: parseNumber(c); break;
+                case PendingTokenType::name: parseName(c); break;
+                case PendingTokenType::add_op: parseAddOp(c); break;
+                case PendingTokenType::sub_op: parseSubOp(c); break;
+                case PendingTokenType::mul_op: parseMulOp(c); break;
+                case PendingTokenType::div_op: parseDivOp(c); break;
+                default:
+                    throw std::runtime_error("not implemented (yet)");
             }
         }
 
-        if (!current.isEmpty())
-            tokens.push_back(current.toToken());
+        static PendingTokenType getType(const char c) {
+            if (isdigit(c)) return PendingTokenType::number;
+            if (isalpha(c)) return PendingTokenType::name;
+            if (c == ' ') return PendingTokenType::none;
+            if (c == '\'' || c == '"') return PendingTokenType::string;
+            if (c == '+') return PendingTokenType::add_op;
+            if (c == '-') return PendingTokenType::sub_op;
+            if (c == '*') return PendingTokenType::mul_op;
+            if (c == '/') return PendingTokenType::div_op;
+            return PendingTokenType::unknown;
+        }
 
-        return tokens;
-    }
+        void parseNone(const char c) {
+            const auto type = getType(c);
+            switch (getType(c)) {
+                case PendingTokenType::number:
+                case PendingTokenType::name:
+                case PendingTokenType::add_op:
+                case PendingTokenType::sub_op:
+                case PendingTokenType::div_op:
+                case PendingTokenType::mul_op:
+                    previous.type = type;
+                    previous.value += c;
+                    break;
+                case PendingTokenType::string:
+                case PendingTokenType::unknown:
+                    previous.type = type;
+                    break;
+                case PendingTokenType::none:
+                    break;
+            }
+        }
+
+        void parseString(const char c) {
+            if (c == '\'' || c == '"') {
+                tokens.push_back(previous.toToken());
+                previous.clear();
+                return;
+            }
+
+            previous.value += c;
+        }
+
+        void parseNumber(const char c) {
+            const auto type = getType(c);
+            if (type == PendingTokenType::number) {
+                previous.value += c;
+                return;
+            }
+
+            tokens.push_back(previous.toToken());
+            previous.clear();
+            parseChar(c);
+        }
+
+        void parseName(const char c) {
+            const auto type = getType(c);
+            if (type == PendingTokenType::name || type == PendingTokenType::number) {
+                previous.value += c;
+                return;
+            }
+
+            tokens.push_back(previous.toToken());
+            previous.clear();
+            parseChar(c);
+        }
+
+        void parseAddOp(const char c) {
+            tokens.push_back(previous.toToken());
+            previous.clear();
+            parseChar(c);
+        }
+
+        void parseSubOp(const char c) {
+            tokens.push_back(previous.toToken());
+            previous.clear();
+            parseChar(c);
+        }
+
+        void parseMulOp(const char c) {
+            tokens.push_back(previous.toToken());
+            previous.clear();
+            parseChar(c);
+        }
+
+        void parseDivOp(const char c) {
+            tokens.push_back(previous.toToken());
+            previous.clear();
+            parseChar(c);
+        }
+    };
 }
 
 #endif //SOVIET_TOKENIZER_HPP
