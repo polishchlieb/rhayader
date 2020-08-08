e2dbd175b70148e6db5b73dd9cbd1ca45291e596
polishchlieb <biczu.yt@gmail.com>
2020-08-09 00:09:06 +0200

    added basic debugger

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include "tokenizer/Tokenizer.hpp"
 #include "parser/Parser.hpp"
+#include "parser/dumpNode.hpp"
 #include "evaluator/evaluator.hpp"
 
 int main() {
-    tokenizer::Tokenizer t;
-    const auto tokens = t.tokenize("(2 + 2) * 2");
+    while (true) {
+        std::cout << "Input: ";
+        std::string input;
+        std::getline(std::cin, input);
+        std::cout << "---------------------" << std::endl;
 
-    parser::Parser p;
-    const auto rootNode = p.parse(tokens);
+        if (input == "exit") {
+            break;
+        }
 
-    float value = evaluator::evaluate(rootNode);
-    std::cout << value << std::endl;
+        tokenizer::Tokenizer t;
+        const auto tokens = t.tokenize(input);
+
+        std::cout << "Tokenizer output:" << std::endl;
+        for (const auto& token : tokens) {
+            std::cout << "(" << tokenizer::dumpTokenType(token.type) << ") " << token.value << std::endl;
+        }
+        std::cout << "---------------------" << std::endl;
+
+        parser::Parser p;
+        const auto rootNode = p.parse(tokens);
+        std::cout << "Parser output:" << std::endl;
+        parser::dump(rootNode);
+        std::cout << "---------------------" << std::endl;
+
+        std::cout << "Evaluator output:" << std::endl;
+        float value = evaluator::evaluate(rootNode);
+        std::cout << value << std::endl << std::endl;
+    }
     return 0;
 }
